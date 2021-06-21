8698d7bfe753cca6172475ecdd96b4014bf31101
Dlzheen Mahmood <dlzheenmahmood@gmail.com>
2021-06-21 19:04:38 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Game from './components/Game';
 import './style.css';
-
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
